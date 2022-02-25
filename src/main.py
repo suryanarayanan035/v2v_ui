cb5373482e2260b88c22bc9c2fe6e216ba23b54c
rohith2407 <rohithvishwavs@gmail.com>
2022-02-25 14:08:33 +0530

    changed button name of the ui

--- src/main.py
+++ src/main.py
@@ -9,5 +9,5 @@ win.geometry("900x700")
 #Text shown in the header
 Label(win, text= "V2V",fg='#b45', font= ('Aerial 17 bold italic')).pack(pady= 30)
 #button
-ttk.Button(win, text= "connect to cyton board", command=threading.Thread(print("u have clicked the button")).start()).pack(pady= 40)
+ttk.Button(win, text= "start streaming", command=threading.Thread(print("streaming started")).start()).pack(pady= 40)
 win.mainloop()
