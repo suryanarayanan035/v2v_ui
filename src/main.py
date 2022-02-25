58b298f1d16744fbd0005a1c814aaa264fb8f131
rohith2407 <rohithvishwavs@gmail.com>
2022-02-25 15:23:07 +0530

    checked data streaming

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
+import sys
+sys.path.append("../")
 from cProfile import label
 from cgitb import text
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 import threading
+from board.index import Board
 win=Tk()
+board=Board.getBoard()
 win.geometry("900x700")
 #Text shown in the header
 Label(win, text= "V2V",fg='#b45', font= ('Aerial 17 bold italic')).pack(pady= 30)
 #button
-ttk.Button(win, text= "start streaming", command=threading.Thread(print("streaming started")).start()).pack(pady= 40)
+ttk.Button(win, text= "start streaming", command=threading.Thread(target=board.start_streaming).start()).pack(pady= 40)
 win.mainloop()
+
